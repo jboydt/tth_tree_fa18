b78cc1ed2544afd5d2a080d5f44a66d35a1cc12f
GLRammer <45434125+GLRammer@users.noreply.github.com>
2018-12-03 10:49:12 -0800

    added file opener

--- driver.cpp
+++ driver.cpp
@@ -1,5 +1,28 @@
 // TODO
 
-// load file 
+// load file
 // parse commands
 // apply commands to a tree
+#include <fstream>
+#include <functional>
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[]){
+	if (argc!=2){
+		cout << "Usage: " << argv[0] << " FILENAME\n";
+	}else{
+    string filename=argv[1];
+		bool running=true;
+		unsigned int temp;
+		ifstream inputFile;
+		while (running){
+			inputFile.open(filename);
+			if (inputFile.is_open()){
+				string nextLine;
+      }else {
+        cout << "Unable to find/read file.\n";
+        running=false;
+      }
+    }
+  }
+}
