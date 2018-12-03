d1b2d4a4f24415874f9368effcdd24ae2f3a847d
GLRammer <45434125+GLRammer@users.noreply.github.com>
2018-12-03 11:09:53 -0800

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -19,6 +19,39 @@ int main(int argc, char* argv[]){
 			inputFile.open(filename);
 			if (inputFile.is_open()){
 				string nextLine;
+        BSTree<Word>* tree=nullptr;
+        while (getline(inputFile, nextLine)){
+					if (nextLine.at(0) == '#'){
+					}else if (nextLine.at(0)=='C'){
+            if (tree!=nullptr)
+              delete tree;
+            cout << "TREE CREATED\n";
+            tree=new BSTree<Word>;
+          }else if (nextLine.at(0)=='X'){
+            if (tree!=nullptr){
+							tree->clear();
+							cout << "TREE CLEARED\n";
+						}else{
+							cout << "MUST CREATE TREE INSTANCE\n";
+						}
+          }else if (nextLine.at(0)=='D'){
+            if (tree!=nullptr){
+							delete tree;
+							cout << "TREE DELETED\n";
+						}else{
+							cout << "MUST CREATE TREE INSTANCE\n";
+						}
+          }else if (nextLine.at(0)=='I'){
+            if (tree!=nullptr){
+              string y=nextLine.substr(2, 999);
+							if (tree->find(y)){
+                
+              }
+							cout << "TREE DELETED\n";
+						}else{
+							cout << "MUST CREATE TREE INSTANCE\n";
+						}
+          }
       }else {
         cout << "Unable to find/read file.\n";
         running=false;
