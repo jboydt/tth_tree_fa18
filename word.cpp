d916dd797debc3d84d21665e26bf18c1948a69ed
chrisdoesmyhair <chrisdoesmyhair@gmail.com>
2018-12-04 12:50:55 -0800

    worked on word and its functions. uppercase can be double checked

--- word.cpp
+++ word.cpp
@@ -1,28 +1,29 @@
 #include "word.h"
 
 Word::Word() {
-
+  unsinged int amount = 0;
+  string name;
 }
 
 Word::Word(string newWord) {
-
+  name = newWord;
+  amount = 0;
 }
 
 void Word::setWord(string newWord) {
-
+  name = newWord;
 }
 
-void Word::incrementCount(unsigned int amount)
-{
-
+void Word::incrementCount(unsigned int amount){
+    amount++;
 }
 
 string Word::getWord()  {
-
+  return uppercase(name);
 }
 
 unsigned int Word::getCount()  {
-
+  return amount;
 }
 
 bool Word::operator<(const Word& otherWord)  {
@@ -39,4 +40,9 @@ bool Word::operator==(const Word& otherWord)  {
 
 ostream& operator<<(ostream& outs, const Word& w)  {
 
+}
+string Word::uppercase(string s){
+  for(int i =0, i < s.size(), i++){
+    s.at(i) = toupper(s.at(i));
+  }
 }
